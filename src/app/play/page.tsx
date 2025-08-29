06166ce2a19cdcb17067350d9b7cdc6bff3146d2
chat263 <130237209+chat263@users.noreply.github.com>
2025-08-29 17:45:14 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1334,10 +1334,11 @@ function PlayPageClient() {
             const hls = new Hls({
               debug: false, // 关闭日志
               enableWorker: true, // WebWorker 解码，降低主线程压力
-              lowLatencyMode: true, // 开启低延迟 LL-HLS
+              lowLatencyMode: false, // 开启低延迟 LL-HLS
 
               /* 缓冲/内存相关 */
               maxBufferLength: 600, // 前向缓冲最大 30s，过大容易导致高延迟
+              maxMaxBufferLength: 3000,
               backBufferLength: 300, // 仅保留 30s 已播放内容，避免内存占用
               maxBufferSize: 900 * 1000 * 1000, // 约 60MB，超出后触发清理
 
