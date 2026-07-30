99d35142cba0a5d1ae7cfd94dc55e547a39b714d
chat263 <130237209+chat263@users.noreply.github.com>
2026-07-31 01:02:21 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,7 +113,7 @@ export default async function RootLayout({
         />
       </head>
       <body
-        className={`${inter.variable} min-h-screen bg-white text-gray-900 dark:bg-black dark:text-gray-200`}
+        className={`${inter.className} min-h-screen bg-white text-gray-900 dark:bg-black dark:text-gray-200`}
       >
         <ThemeProvider
           attribute='class'
